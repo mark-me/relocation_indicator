cb25ae5504ab0a3ef6807cb28a0856133267792d
mark-me <mark_zwart76@graydon-moving-indicator.c.graydon-moving-indicator.internal>
2018-12-12 15:33:55 +0000

    Split inspection

--- Notebooks/modules/common.py
+++ Notebooks/modules/common.py
@@ -2,9 +2,6 @@ import sys, os
 import gcsfs
 from google.cloud import storage
 
-def get_dir():
-  return sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 class GC_Data_Processing(object):
 
     def __init__(self, name_project, name_bucket):
@@ -14,10 +11,10 @@ class GC_Data_Processing(object):
         self.gc_bucket = cs.get_bucket(name_bucket)
 
     def get_gc_bucket(self):
-        return self.bucket
+        return self.gc_bucket
 
     def get_gc_fs(self):
-        return self.fs
+        return self.gc_fs
 
     def upload_blob(self, name_file_source, name_blob_destination):
         """Uploads a file to the Google Cloud Bucket."""
